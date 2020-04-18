d3a121a3010dd46cb70116bb02a6d26cd7a1159f
huahaoxin <2416681914@qq.com>
2020-04-18 12:00:04 +0800

    Revert "第二次提交课室页面，增加了筛选，还有日期调用不出来"
    This reverts commit aea1bfff053a66d2de4d65760693522b08d68670.

--- miniprogram/pages/classroom/classroom.js
+++ miniprogram/pages/classroom/classroom.js
@@ -6,52 +6,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    option1: [
-      { text: '文科楼', value: 0 },
-      { text: '理科楼', value: 1 },
-      { text: '信息楼', value: 3 },
-      { text: '伟南楼', value: 4 }
-    ],
-    option2: [
-      { text: '开始时间', value: 'a1' },
-      { text: '8：00', value: 'a' },
-      { text: '9：00', value: 'b' },
-      { text: '10：00', value: 'c' },
-      { text: '11：00', value: 'd' },
-      { text: '12：00', value: 'e' },
-      { text: '13：00', value: 'f' },
-      { text: '14：00', value: 'g' },
-      { text: '15：00', value: 'f' },
-      { text: '16：00', value: 'i' },
-      { text: '17：00', value: 'j' },
-      { text: '18：00', value: 'k' },
-      { text: '19：00', value: 'l' },
-      { text: '20：00', value: 'm' },
-      { text: '21：00', value: 'n' },
-      { text: '22：00', value: 'o' }
-
-    ],
-    option3: [
-      { text: '结束时间', value: 'a1' },
-      { text: '8：00', value: 'a' },
-      { text: '9：00', value: 'b' },
-      { text: '10：00', value: 'c' },
-      { text: '11：00', value: 'd' },
-      { text: '12：00', value: 'e' },
-      { text: '13：00', value: 'f' },
-      { text: '14：00', value: 'g' },
-      { text: '15：00', value: 'f' },
-      { text: '16：00', value: 'i' },
-      { text: '17：00', value: 'j' },
-      { text: '18：00', value: 'k' },
-      { text: '19：00', value: 'l' },
-      { text: '20：00', value: 'm' },
-      { text: '21：00', value: 'n' },
-      { text: '22：00', value: 'o' }
-    ],
-    value1: 0,
-    value2: 'a1',
-    value3: 'a1',
     value: '',
     classroom: ['文科楼\n101', '文科楼\n102', '文科楼\n103', '文科楼\n104', '文科楼\n105', '文科楼\n106', '文科楼\n107', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201', '文科楼\n108', '文科楼\n201']
   },
